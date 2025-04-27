f561677001ed9ca13cdcc67aac68a11b54247953
Jigar1992 <jigar.thakkar@tatvasoft.com>
2025-04-27 08:36:48 +0530

    Push

--- MyFirstMVCApplication/Controllers/RegisterController.cs
+++ MyFirstMVCApplication/Controllers/RegisterController.cs
@@ -16,12 +16,23 @@ namespace MyFirstMVCApplication.Controllers
         {
             RegisterModel registerModel = new RegisterModel();
             registerModel.FirstName = "Jigar";
+
+            List<UserModel> userModel = new List<UserModel> { new UserModel() { UserID = 1, UserName = "Jigar" },
+             new UserModel() { UserID = 2, UserName = "Himani" },
+            new UserModel() { UserID = 3, UserName = "Hitaxi" },
+            new UserModel() { UserID = 4, UserName = "Nishit" },
+            new UserModel() { UserID = 5, UserName = "Foram" }};
+
+            ViewBag.UserList = userModel;
+            registerModel.UserList = userModel;
+
             return View(registerModel);
         }
 
         [HttpPost] // Data save
-        public ActionResult Index(RegisterModel registerModel)
+        public ActionResult Save(RegisterModel registerModel)
         {
+            
             //RegisterModel registerModel = new RegisterModel();
             //registerModel.FirstName = "Jigar";
             //Save into database
--- MyFirstMVCApplication/Models/RegisterModel.cs
+++ MyFirstMVCApplication/Models/RegisterModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,22 @@ namespace MyFirstMVCApplication.Models
 {
     public class RegisterModel
     {
-        public int UserId { get; set; }
+        public int UserId { get; set; } = 10;
         public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        [DisplayName("First Name ")] // Annotation
         public string FirstName { get; set; }
+
+        [DisplayName("Last Name ")] // Annotation
+        public string LastName { get; set; }
+
+
+        public bool IsActive { get; set; } = true;
+
+        public string Gender { get; set; }
+
+        public List<UserModel> UserList { get; set; }
     }
 }
